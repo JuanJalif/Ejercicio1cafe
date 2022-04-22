69b935b5652c212a36c5469445816f6ab4e65ad9
JuanJalif <104107242+JuanJalif@users.noreply.github.com>
2022-04-22 19:52:52 -0300

    Initial commit

---
+++ src/Ejercicio1cafe.ts
@@ -0,0 +1,8 @@
+console.log("Como hacer un cafe");
+
+console.log("Poner el cafe en una taza");
+console.log("Ponerle X cantidad de azucar (opcional)");
+console.log("Revolver el cafe con un poco de agua");
+console.log("Colocar agua a 90 grados");
+console.log("Revolver con una cuchara");
+console.log("Consumirlo");
